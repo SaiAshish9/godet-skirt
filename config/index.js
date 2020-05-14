504e1d055d1f2a5553ed08d6281b9a99acd34f31
Roopin Bhadri <bhadriroopin748@gmail.com>
2020-05-15 03:03:50 +0530

    Initial commit

--- config/index.js
+++ config/index.js
@@ -22,14 +22,43 @@ export default {
     "sewing pattern"
   ],
   optionGroups: {
-    fit: ["size"]
+    Type:["skirttype"],
+    Hem: ["manipulateHem"],
+    Dart: [
+      "dartGap","waistDartPosition",
+      {Front:["frontLeftDartLength","frontRightDartLength","frontDartWidth"]},
+      {Back:["backLeftDartLength","backRightDartLength","backDartWidth"]}
+    ],
   },
-  measurements: [],
-  dependencies: {},
+  measurements: ["length","frontWaistArc","backWaistArc","hemLine","frontHipArc","backHipArc","naturalWaist","hipsCircumference"],
+  dependencies: {},  
   inject: {},
   hide: [],
-  parts: ["box"],
+  parts: ["back","front","godet"],
   options: {
-    size: { pct: 50, min: 10, max: 100 }
+    manipulateHem: { mm: 0, min: -50.8, max: 50.8 },
+    frontLeftDartLength: { mm: 86.4, min: 35.6, max: 137.2 },
+    frontRightDartLength: { mm: 86.4, min: 35.6, max: 137.2 },
+    backLeftDartLength: { mm: 139.7, min: 88.9, max: 190.5 },
+    backRightDartLength: { mm: 127, min: 76.2, max: 177.8 },
+    frontDartWidth: { mm: 25.4, min: 0, max: 63.5 },
+    backDartWidth: { mm: 25.4, min: 0, max: 69.8 },
+    waistDartPosition: { mm: 86.4, min: 35.6, max: 137.2 },
+    naturalWaistToHip: { mm: 228.6, min: 177.8, max: 279.4 },
+    dartGap: { mm: 25.4, min: 12.7, max: 50.8 },
+    skirttype: {
+      list: ['aline','basic','panel','godet'],
+      dflt: 'godet'
+    },
+    panelLength: { mm: 533.4, min: 406.4, max: 660.4 },
+    waistBandWidth: { mm: 76.2, min: 25.4, max: 127 },
+    numOfPanels: { count: 8, min: 4, max: 14 },
+    naturalWaistToHip: { mm: 203.2, min: 127, max: 279.4 },
+    hemExcess: { mm: 0, min: -76.2, max: 203.2 },
+    numOfGodets: { count: 6, min: 2, max: 10 },
+    godetWidth: { mm: 101.6, min: 50.8, max: 152.4 },
+    godetLength: { mm: 254, min: 177.8, max: 330.2 },
+    frontGodetGap: { mm: 25.4, min: 12.7, max: 50.8 },
+    backGodetGap: { mm: 38.1, min: 12.7, max: 50.8 }
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import freesewing from '@freesewing/core'
 import plugins from '@freesewing/plugin-bundle'
 import config from '../config'
-import draftBox from './box'
+import draftFront from './front'
+import draftBack from './back'
+import draftGodet from './godet'
 
 // Create new design
 const Pattern = new freesewing.Design(config, plugins)
 
 // Attach the draft methods to the prototype
-Pattern.prototype.draftBox = draftBox
+Pattern.prototype.draftFront = draftFront
+Pattern.prototype.draftBack = draftBack
+Pattern.prototype.draftGodet = draftGodet
 
 export default Pattern
